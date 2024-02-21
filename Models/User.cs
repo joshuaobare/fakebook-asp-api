1a210fbf10b4e90327eae9ab580cdaf3c52649cc
joshuaobare <jobare.obare@gmail.com>
2024-02-21 15:21:56 +0300

    create User model

---
+++ Models/User.cs
@@ -0,0 +1,21 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace fakebook_asp_api.Models;
+
+public class User{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string Username { get; set; } = null!;
+    public string FullName { get; set; } = null!;
+    public string Email { get; set; } = null!;
+    public string Password { get; set; } = null!;
+    public DateTime JoinedAt { get; set; } = DateTime.Now;
+    public string Avatar { get; set; } = null!;
+    public string Bio { get; set; } = null!;
+    public string JobTitle { get; set; } = null!;
+    public string HomeLocation { get; set; } = null!;
+    public string RelationshipStatus { get; set; } = null!;
+    public string[] Friends { get; set; } = Array.Empty<string>();
+    public string[] FriendRequests { get; set; } = Array.Empty<string>();
+}
