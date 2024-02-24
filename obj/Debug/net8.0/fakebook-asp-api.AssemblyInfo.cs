9ba0c78b1de2d9b651f41cc5e468800f8cc29c8a
joshuaobare <jobare.obare@gmail.com>
2024-02-24 13:06:01 +0300

    create PostService file and add comments to Program.cs file

--- obj/Debug/net8.0/fakebook-asp-api.AssemblyInfo.cs
+++ obj/Debug/net8.0/fakebook-asp-api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("fakebook-asp-api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5cfee428286d64bfabd79ebdfa85cd751a341e5d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c53b926d942ca4e5b7af83e30262711dac3c643e")]
 [assembly: System.Reflection.AssemblyProductAttribute("fakebook-asp-api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("fakebook-asp-api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
