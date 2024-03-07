df4543d5369ab45a5cf18e224132c87c295e70bb
joshuaobare <jobare.obare@gmail.com>
2024-03-07 19:29:12 +0300

    add controller functions to UserController file

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -5,10 +5,67 @@ using fakebook_asp_api.Services;
 
 namespace fakebook_asp_api.Controllers;
 
-public class UserController : Controller
-{
-    public IActionResult Index()
+[ApiController]
+[Route("api/[controller]")]
+public class UserController : Controller {
+    private readonly UserService _userService;
+
+    public UserController(UserService userService) => _userService = userService;
+
+    [HttpGet]
+    public async Task<List<User>> Get() =>
+        await _userService.GetUsersAsync();
+
+    [HttpGet("{id:length(24)}")]
+    public async Task<ActionResult<User>> Get(string id)
     {
-        return View();
+        var user = await _userService.GetUserAsync(id);
+
+        if (user is null)
+        {
+            return NotFound();
+        }
+        return user;
+
     }
+
+    public async Task<IActionResult> Post(User newUser)
+    {
+        await _userService.CreateAsync(newUser);
+
+        return CreatedAtAction(nameof(Get), new { id = newUser.UserId }, newUser);
+    }
+
+    [HttpPut("{id:length(24)}")]
+    public async Task<IActionResult> Update(string id, User updatedUser)
+    {
+        var user = await _userService.GetUserAsync(id);
+
+        if (user is null)
+        {
+            return NotFound();
+        }
+
+        updatedUser.UserId = user.UserId;
+
+        await _userService.UpdateAsync(id, updatedUser);
+
+        return NoContent();
+    }
+
+    [HttpDelete("{id:length(24)}")]
+    public async Task<IActionResult> Delete(string id)
+    {
+        var user = await _userService.GetUserAsync(id);
+
+        if (user is null)
+        {
+            return NotFound();
+        }
+
+        await _userService.RemoveAsync(id);
+
+        return NoContent();
+    }
+
 }
